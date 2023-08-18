b66b667b7a179c97d137e55d6b8d873598b3aefb
ezzx1 <100594057+ezzx1@users.noreply.github.com>
2023-08-18 16:41:32 +0300

    Update urls.py

--- tubex2/urls.py
+++ tubex2/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-from jet_django.urls import jet_urls
 
 urlpatterns =[
     
